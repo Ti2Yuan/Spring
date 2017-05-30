49e7bb540b7405e9f6ec1848209f54f6442b2efa
Ti2Yuan <chenti275001068@gmail.com>
2017-05-30 13:59:54 +0800

    增加文件上传

--- SSMDemo/src/main/java/com/chen/ssmdemo/controllers/GoodsController.java
+++ SSMDemo/src/main/java/com/chen/ssmdemo/controllers/GoodsController.java
@@ -3,7 +3,11 @@
  */
 package com.chen.ssmdemo.controllers;
 
+import java.io.File;
+import java.util.UUID;
+
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
@@ -13,11 +17,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.chen.ssmdemo.entities.Goods;
 import com.chen.ssmdemo.services.GoodsService;
-import com.sun.org.apache.bcel.internal.generic.NEW;
 
 /**
  * @author chenti
@@ -98,12 +102,92 @@ public class GoodsController {
 		return "goods/add";
 	}
 	
+	/**
+	 * 编辑商品
+	 * */
+	@RequestMapping("/edit/{id}")
+	public String edit(Model model, @PathVariable int id){
+		model.addAttribute("good", goodsService.getGoodsById(id));
+		return "goods/edit";
+	}
 	
+	/**
+	 * 编辑保存
+	 * TODO
+	 * @param model
+	 * @param good
+	 * @param bindingResult  Spring 验证的错误返回
+	 * @return
+	 * String
+	 */
+	@RequestMapping("/editSave")
+	public String editSave(Model model, @ModelAttribute("good") @Valid Goods good, BindingResult bindingResult){
+		if(!bindingResult.hasErrors()){
+			if(goodsService.update(good) > 0){
+				return "redirect:list";
+			}
+		}
+		//如果模型中存在错误
+		model.addAttribute("good", good);
+		return "/goods/edit";
+	}
 	
+	/**
+	 * 上传图片
+	 * TODO
+	 * @param model
+	 * @param id
+	 * @return
+	 * String
+	 */
+	@RequestMapping("/upPicture/{id}")
+	public String upPicture(Model model, @PathVariable int id){
+		model.addAttribute("good", goodsService.getGoodsById(id));
+		return "goods/upfile";
+	}
 	
-	
-	
-	
+	/**
+	 * 上传图片保存
+	 * TODO
+	 * @param model
+	 * @param id
+	 * @return
+	 * String
+	 */
+	@RequestMapping("/upPictureSave/{id}")
+	public String upPictureSave(Model model, @PathVariable int id, MultipartFile picFile, HttpServletRequest request){
+		Goods good = goodsService.getGoodsById(id);
+		
+		//如果选择了文件
+		if(picFile != null){
+			//如果文件大小不为0
+			if(picFile.getSize() > 0){
+				//活得上传位置
+				String path = request.getServletContext().getRealPath("/images");
+				System.out.println("path: "+path);
+				//生成文件名
+				String str = picFile.getOriginalFilename();
+				System.out.println(str);
+				String filename = UUID.randomUUID().toString()+picFile.getOriginalFilename().substring(picFile.getOriginalFilename().lastIndexOf("."));
+				File tempFile = new File(path, filename);
+				try {
+					//保存文件
+					picFile.transferTo(tempFile);
+					//更新数据
+					good.setPicture(filename);
+					goodsService.update(good);
+					
+					return "redirect:/goods/list";
+				} catch (Exception e) {
+					// TODO: handle exception
+					e.printStackTrace();
+				}
+				
+			}
+		}
+		model.addAttribute("good", good);
+		return "goods/upfile";
+	}
 	
 	
 	
--- SSMDemo/src/main/webapp/WEB-INF/views/jstl/goods/upfile.jsp
+++ SSMDemo/src/main/webapp/WEB-INF/views/jstl/goods/upfile.jsp
@@ -1,12 +1,42 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+	<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link href="<c:url value="/styles/main.css"/>" type="text/css"
+	rel="stylesheet" />
+<title>上传图片</title>
 </head>
 <body>
+	<div class="main">
+		<h2 class="title">
+			<span>上传图片</span>
+		</h2>
+		<form action="<c:url value="/goods/upPictureSave/${good.id}" />"
+			method="post" enctype="multipart/form-data">
+			<fieldset>
+				<legend>商品</legend>
+				<p>
+					<label for="name">商品名称：</label> ${good.name}
+				</p>
+				<p>
+					<label for="price">商品价格：</label>${good.price}
+				</p>
+				<p>
+					<label for="title">商品图片：</label> <input type="file" name="picFile" />
+				</p>
 
+				<p>
+					<input type="submit" value="上传" class="btn out">
+				</p>
+			</fieldset>
+		</form>
+		<p style="color: red">${message}</p>
+		<p>
+			<a href="<c:url value="/goods/list" />" class="abtn out">返回列表</a>
+		</p>
+	</div>
 </body>
 </html>
